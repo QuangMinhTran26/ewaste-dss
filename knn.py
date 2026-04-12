5e7c7638c6cd117e7cedab2b6cd2a4e8e6c6d8bf
Absolutiom <minh.sthk@gmail.com>
2026-04-12 02:40:59 +0200

    feat: build kNN model and

--- knn.py
+++ knn.py
@@ -26,3 +26,8 @@ y = df[metals]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
+
+knn = KNeighborsRegressor(n_neighbors=5)
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
