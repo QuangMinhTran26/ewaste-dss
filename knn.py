8030f01f7befa1c2a7be4538362ef9b07759135c
Absolutiom <minh.sthk@gmail.com>
2026-04-12 02:25:38 +0200

    feat: encode variables

--- knn.py
+++ knn.py
@@ -1,2 +1,10 @@
 import pandas as pd
 df = pd.read_csv("updated_e_waste_dataset.csv")
+
+# Encode Device Condition
+condition_map = {'Broken': 0, 'Average': 1, 'Good': 2}
+df['Condition_encoded'] = df['Device Condition'].map(condition_map)
+
+# Encode Device Type
+device_type_map = {'Appliance': 0, 'Consumer Electronics': 1, 'IT Equipment': 2}
+df['DeviceType_encoded'] = df['Device Type'].map(device_type_map)
