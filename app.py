6977be773dee5b70b9768dcfb2ec4a6538a4b2cd
Absolutiom <minh.sthk@gmail.com>
2026-04-13 02:55:03 +0200

    feat: encode condition and device type; select variables

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("updated_e_waste_dataset.csv")
+
+condition_map = {'Broken': 0, 'Average': 1, 'Good': 2}
+device_type_map = {'Appliance': 0, 'Consumer Electronics': 1, 'IT Equipment': 2}
+
+df['Condition_encoded'] = df['Device Condition'].map(condition_map)
+df['DeviceType_encoded'] = df['Device Type'].map(device_type_map)
+
+metals = ['Gold (g)', 'Silver (g)', 'Aluminum (g)',
+          'Platinum (g)', 'Nickel (g)', 'Tin (g)',
+          'Lithium (g)', 'Rhodium (g)']
+
+features = ['Condition_encoded', 'DeviceType_encoded',
+            'Year of Manufacture', 'Device Age',
+            'Market Value of Metals']
