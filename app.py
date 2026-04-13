4facaafd2dac06cc6553e835f87c91c984e49e7e
Absolutiom <minh.sthk@gmail.com>
2026-04-13 02:55:37 +0200

    feat: train

--- app.py
+++ app.py
@@ -18,3 +18,10 @@ metals = ['Gold (g)', 'Silver (g)', 'Aluminum (g)',
 features = ['Condition_encoded', 'DeviceType_encoded',
             'Year of Manufacture', 'Device Age',
             'Market Value of Metals']
+
+X = df[features]
+y = df[metals]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+knn = KNeighborsRegressor(n_neighbors=5)
+knn.fit(X_train, y_train)
